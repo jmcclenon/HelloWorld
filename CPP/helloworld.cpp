e8bb5c33d729fcacc57c5f62ba923ddc54e86998
John McClenon Jr <mcclenon.m@gmail.com>
2016-10-30 14:38:53 -0400

    helloworld.cpp

--- CPP/helloworld.cpp
+++ CPP/helloworld.cpp
@@ -1,30 +1,8 @@
-//helloworld.cpp
-//McClenon, John M.
-//10/30/2016.
-//Created on GitHub
-#include<iostream>
-using namespace std;
-//Definition of the class Hello2016.
-class Hello2016
-{
-public:
-	void imprime() 
-	{
-		cout << "\n\n\n\aWelcome to C++ 2016! \n\n";
-		cout << "\t\t\aHello, This is my first program.\n\n\n";
-    }
-	void imprime2() 
-	{ 
-		cout << "\t\t\aThere's more where this come from...\n\n";
-		cout << "\t\t\aStick around to see even more, Thanks!\n\n";
-	} 
-};
-//For printing a message trough the screen.
+#include 
+ 
 int main()
 {
-	Hello2016 hello;
-	hello.imprime();
-	hello.imprime2();
-	system("pause");
-	return 0;
+    std::cout << "Hello, world!
+";
+    return 0;
 }
